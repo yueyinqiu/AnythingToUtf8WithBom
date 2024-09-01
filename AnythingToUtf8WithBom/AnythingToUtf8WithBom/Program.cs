800bb9dec3ee36dc05109a78eacbf391958e727f
yueyinqiu <yueyinqiu@outlook.com>
2024-09-01 14:20:39 +0800

    atu

---
+++ AnythingToUtf8WithBom/AnythingToUtf8WithBom/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+Console.Write("Directory (e.g. C:/Users/yueyi/Documents): ");
+var directoryInput = Console.ReadLine() ?? "";
+Console.Write("Pattern (e.g. *.txt): ");
+var patternInput = Console.ReadLine() ?? "";
+Console.Write("Input Encoding (e.g. GB2312): ");
+var inputEncodingInput = Console.ReadLine() ?? "";
+
+var inputEncoding = Encoding.GetEncoding(inputEncodingInput);
+var outputEncoding = new UTF8Encoding(true);
+var files = Directory.GetFiles(directoryInput, patternInput, SearchOption.AllDirectories);
+
+Console.WriteLine();
+Console.WriteLine($"Encoding: ");
+Console.WriteLine($"    {inputEncoding.EncodingName} -> {outputEncoding.EncodingName}");
+Console.WriteLine($"Files: ");
+foreach (var file in files)
+    Console.WriteLine($"    {file}");
+Console.Write($"Enter anything to continue: ");
+_ = Console.ReadLine();
+
+foreach (var file in files)
+{
+    Console.WriteLine($"Dealing with {file} ...");
+    var text = await File.ReadAllTextAsync(file, inputEncoding);
+    await File.WriteAllTextAsync(file, text, outputEncoding);
+}
